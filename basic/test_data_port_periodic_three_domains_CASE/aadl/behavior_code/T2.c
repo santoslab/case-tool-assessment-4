48560dcdcaae4bb79ab750059b26bd81fbc6cb10
Jason Belt <belt@ksu.edu>
2024-08-22 13:21:01 -0500

    add CASE version

---
+++ basic/test_data_port_periodic_three_domains_CASE/aadl/behavior_code/T2.c
@@ -0,0 +1,24 @@
+#include <camkes.h>
+#include <stdio.h>
+#include <sb_types.h>
+#include <sb_consumer_thread_i.h>
+
+void init(const int64_t *in_arg) {
+  printf("[%s] init called\n", get_instance_name());
+}
+
+void compute(const int64_t * in_arg) {
+  int8_t _value;
+  
+  if (sb_read_port_read(&value)) {
+    printf("[%s] value {%d}\n", get_instance_name(), value);
+
+    if (sb_write_port_write( &_value )) {
+      printf("[%s] Sent %d\n", get_instance_name(), _value);
+    } else {
+      printf("[%s] Unable to send\n", get_instance_name());
+    }
+  } else {
+    printf("[%s] no value consumed.\n", get_instance_name());
+  }
+}
